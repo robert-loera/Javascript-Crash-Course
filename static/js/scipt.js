ffcbf83a9acaadbc8a75f5f738a93af5c9d3ef83
robert-loera <82541496+robert-loera@users.noreply.github.com>
2021-09-26 18:10:52 -0500

    added cards

--- static/js/scipt.js
+++ static/js/scipt.js
@@ -146,3 +146,21 @@ function buttonsRandom(){
     all_buttons[i].classList.add(choices[randNumber])
   }
 }
+
+//Challenge 5: Blackjack
+
+let blackjackGame = {
+  'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
+  'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0}
+}
+
+const YOU = blackjackGame['you']
+const DEALER = blackjackGame['dealer']
+
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit)
+
+function blackjackHit(){
+  let cardImage = document.createElement('img')
+  cardImage.src = 'static/images/Q.png'
+  document.querySelector(YOU['div']).appendChild(cardImage)
+}
